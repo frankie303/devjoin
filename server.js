8c350176adc8560ddcf84c546080f98bb7942bd2
mert <uzeyirmertciflikli@gmail.com>
2020-06-26 18:14:06 +0200

    route files added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const { connect } = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +8,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
